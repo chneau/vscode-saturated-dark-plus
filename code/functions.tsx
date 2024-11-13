1b1c0d5bf4e999d4587507171cbe0bfcab596d4e
chneau <charles63500@gmail.com>
2024-11-13 18:02:14 +0000

    Refactor theme saturation logic and update dependencies

--- code/functions.tsx
+++ code/functions.tsx
@@ -7,6 +7,7 @@ export const downloadTheme = async (themeName: string) => {
 	const response = await fetch(url);
 	const text = await response.text();
 	const theme = parse<Theme>(text);
+	theme.tokenColors ??= [];
 	return theme;
 };
 export const fuseThemes = (...themes: Theme[]) => {
@@ -28,28 +29,33 @@ export const fuseThemes = (...themes: Theme[]) => {
 	}
 	return fusedTheme;
 };
+const colorToHsva = (color: Color<string>) => {
+	const [h, s, v, a] = color.hsv().array();
+	if (h === undefined || s === undefined || v === undefined) {
+		throw new Error(`Invalid color: ${color}`);
+	}
+	return [h, s, v, a ?? 1] as const;
+};
 export const saturateTheme = (theme: Theme) => {
 	const saturatedTheme: Theme = { ...theme, name: "Saturated Dark+" };
 	for (const key in saturatedTheme.colors) {
 		const color = Color(saturatedTheme.colors[key]);
-		const [h, s, v, a] = color.hsv().array();
+		const [h, s, v, a] = colorToHsva(color);
 		saturatedTheme.colors[key] = color.hsv(h, s * 1.5, v, a).hexa();
 	}
 	for (const key in saturatedTheme.semanticTokenColors) {
 		const color = Color(saturatedTheme.semanticTokenColors[key]);
-		const [h, s, v, a] = color.hsv().array();
+		const [h, s, v, a] = colorToHsva(color);
 		saturatedTheme.semanticTokenColors[key] = color
 			.hsv(h, s * 1.5, v, a)
 			.hexa();
 	}
-	for (const key in saturatedTheme.tokenColors) {
-		const hex = saturatedTheme.tokenColors[key].settings.foreground;
+	for (const value of Object.values(saturatedTheme.tokenColors)) {
+		const hex = value.settings.foreground;
 		if (!hex) continue;
 		const color = Color(hex);
-		const [h, s, v, a] = color.hsv().array();
-		saturatedTheme.tokenColors[key].settings.foreground = color
-			.hsv(h, s * 2.3, v, a)
-			.hexa();
+		const [h, s, v, a] = colorToHsva(color);
+		value.settings.foreground = color.hsv(h, s * 2.3, v, a).hexa();
 	}
 	return saturatedTheme;
 };
--- code/index.ts
+++ code/index.ts
@@ -1,24 +1,27 @@
 import { downloadTheme, fuseThemes, saturateTheme } from "./functions";
 import type { Theme } from "./models";
 
+// variables
+const themesToDownload = ["dark_vs", "dark_plus"];
+const tempFile = "default-dark-theme-vscode.json";
+const finalFile = "../themes/Saturated Dark+-color-theme.json";
+
 const start = new Date();
 const msSinceStartInMs = () =>
 	(new Date().getTime() - start.getTime()).toString().padStart(3, " ");
 console.log(`${msSinceStartInMs()}ms: Starting...`);
 
 // download the themes
-const [dark, darkPlus] = await Promise.all([
-	downloadTheme("dark_vs"),
-	downloadTheme("dark_plus"),
-]);
+const darkThemes = await Promise.all(themesToDownload.map(downloadTheme));
 console.log(`${msSinceStartInMs()}ms: Downloaded themes...`);
 
 // fuse everything together
-const theme: Theme = fuseThemes(dark, darkPlus);
+const theme: Theme = fuseThemes(...darkThemes);
 console.log(`${msSinceStartInMs()}ms: Fused themes...`);
 
 // write the theme to disk
-await Bun.write("default-dark-theme-vscode.json", JSON.stringify(theme));
+await Bun.write(tempFile, JSON.stringify(theme));
+await Bun.$`biome format --write ${tempFile}`.quiet().nothrow();
 console.log(`${msSinceStartInMs()}ms: Wrote theme...`);
 
 // create a copy with saturated colors
@@ -30,5 +33,5 @@ const prettyFormat = JSON.stringify(saturatedTheme);
 console.log(`${msSinceStartInMs()}ms: Formatted theme...`);
 
 // write the theme to disk
-await Bun.write("../themes/Saturated Dark+-color-theme.json", prettyFormat);
+await Bun.write(finalFile, prettyFormat);
 console.log(`${msSinceStartInMs()}ms: Wrote saturated theme...`);
